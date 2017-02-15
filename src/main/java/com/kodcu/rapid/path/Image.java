68db1756d33a7d6bf78f535e8555d91dadc9910e
Hakan Ozler <ozler.hakan@gmail.com>
2017-02-15 18:54:40 +0300

    update images api for v1.26

--- src/main/java/com/kodcu/rapid/path/Image.java
+++ src/main/java/com/kodcu/rapid/path/Image.java
@@ -3,6 +3,7 @@ package com.kodcu.rapid.path;
 import com.kodcu.rapid.config.DockerClient;
 
 import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
@@ -20,15 +21,13 @@ public class Image extends DockerClient {
     @GET
     @Path("json")
     @Produces(value = "application/json")
-    public JsonArray getImages(
+    public JsonArray listImages(
             @DefaultValue("false") @QueryParam("all") String all,
-            @QueryParam("filter") String filter,
+            @DefaultValue("false") @QueryParam("digests") String digests,
             @QueryParam("filters") String filters) throws IOException, ExecutionException, InterruptedException {
 
-        WebTarget target = resource().path("images").path("json").queryParam("all", all);
+        WebTarget target = resource().path("images").path("json").queryParam("all", all).queryParam("digests", digests);
 
-        if (Objects.nonNull(filter))
-            target = target.queryParam("filter", filter);
         if (Objects.nonNull(filters))
             target = target.queryParam("filters", URLEncoder.encode(filters, "UTF-8"));
 
@@ -64,21 +63,90 @@ public class Image extends DockerClient {
         return entity;
     }
 
+    @GET
+    @Path("search")
+    public String searchImages(@QueryParam("term") String term,
+                              @QueryParam("limit") int limit,
+                              @QueryParam("filters") String filters)
+            throws IOException, ExecutionException, InterruptedException {
+
+        WebTarget target = resource().path("images").path("search").queryParam("term", term);
+
+        if (limit != 0)
+            target = target.queryParam("limit", limit);
+        if (Objects.nonNull(filters))
+            target = target.queryParam("filters", filters);
+
+        Response response = getResponse(target);
+        String entity = response.readEntity(String.class);
+        response.close();
+        return entity;
+    }
+
+    @POST
+    @Path("commit")
+    public String commitImage(
+            @QueryParam("container") String container,
+            @QueryParam("repo") String repo,
+            @QueryParam("tag") String tag,
+            @QueryParam("comment") String comment,
+            @QueryParam("author") String author,
+            @QueryParam("changes") String changes,
+            @DefaultValue("false") @QueryParam("pause") String pause,
+            JsonObject content) throws IOException, ExecutionException, InterruptedException {
+
+        WebTarget target = resource().path("images").path("commit").queryParam("pause", pause);
+
+        if (Objects.nonNull(container))
+            target = target.queryParam("container", container);
+        if (Objects.nonNull(repo))
+            target = target.queryParam("repo", repo);
+        if (Objects.nonNull(tag))
+            target = target.queryParam("tag", tag);
+        if (Objects.nonNull(comment))
+            target = target.queryParam("comment", comment);
+        if (Objects.nonNull(author))
+            target = target.queryParam("author", author);
+        if (Objects.nonNull(changes))
+            target = target.queryParam("changes", changes);
+
+        Response response = postResponse(target, content);
+        String entity = response.readEntity(String.class);
+        response.close();
+        return entity;
+    }
+
     @POST
     @Path("{id}/tag")
     public String tagImage(
             @PathParam("id") String id,
             @QueryParam("repo") String repo,
-            @DefaultValue("false") @QueryParam("force") String force,
             @QueryParam("tag") String tag) throws IOException, ExecutionException, InterruptedException {
 
         WebTarget target = resource().path("images")
-                .path(id).path("tag")
-                .queryParam("repo", repo)
-                .queryParam("force", force);
+                .path(id).path("tag");
 
         if (Objects.nonNull(tag))
             target = target.queryParam("tag", tag);
+        if (Objects.nonNull(repo))
+            target = target.queryParam("repo", repo);
+
+        Response response = postResponse(target);
+        String entity = response.readEntity(String.class);
+        response.close();
+        return entity;
+    }
+
+    @POST
+    @Path("prune")
+    public String pruneImages(
+            @QueryParam("filters") String filters)
+            throws IOException, ExecutionException, InterruptedException {
+
+        WebTarget target = resource().path("images").path("prune");
+
+        if (Objects.nonNull(filters))
+            target = target.queryParam("filters", filters);
 
         Response response = postResponse(target);
         String entity = response.readEntity(String.class);
@@ -88,7 +156,7 @@ public class Image extends DockerClient {
 
     @DELETE
     @Path("{id}")
-    public String deleteContainer(
+    public String deleteImage(
             @PathParam("id") String id,
             @DefaultValue("false") @QueryParam("force") String force,
             @DefaultValue("false") @QueryParam("noprune") String noprune)
