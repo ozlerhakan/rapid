b5d2db501bde2b6f9171671a17e6597a23a90d2a
Hakan Ozler <ozler.hakan@gmail.com>
2017-03-12 15:27:12 +0300

    update main regex

--- react/src/components/CommandEditor/regex.js
+++ react/src/components/CommandEditor/regex.js
@@ -2,4 +2,4 @@
  * Created by hakan on 11/03/2017.
  */
 
-export const query = /^\s*(GET|POST|PUT|DELETE)[ |\t]+(.*)([\r\n?]?([{\[][\s\S\t ]*[}\]])?)/g;
+export const query = /^\s*(GET|POST|PUT|DELETE)[ |\t]+(.*)[\r\n?]?([{\[][\s\S\t ]*[}\]])?/g;
