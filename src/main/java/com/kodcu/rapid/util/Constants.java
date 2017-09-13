d9e7ecc197addf8008127ab4a0c2c90542877fab
Hakan Ozler <ozler.hakan@gmail.com>
2017-09-13 22:19:42 +0300

    order constants

--- src/main/java/com/kodcu/rapid/util/Constants.java
+++ src/main/java/com/kodcu/rapid/util/Constants.java
@@ -9,12 +9,12 @@ public final class Constants {
     }
 
     public static final String CONTAINERS = "containers";
-    public static final String IMAGES = "images";
     public static final String NETWORKS = "networks";
-    public static final String NODES = "nodes";
-    public static final String MESSAGE = "message";
     public static final String CONFIGS = "configs";
-    public static final String FILTERS = "filters";
+    public static final String IMAGES = "images";
+    public static final String NODES = "nodes";
 
+    public static final String FILTERS = "filters";
+    public static final String MESSAGE = "message";
     public static final String UNIMPLEMENTED = "Unimplemented";
 }
