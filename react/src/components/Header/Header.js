2c3b7e3d29eaba914d38f8f0db9bbe83ec548d54
Hakan Ozler <ozler.hakan@gmail.com>
2017-12-05 18:25:29 +0300

    increment version

--- react/src/components/Header/Header.js
+++ react/src/components/Header/Header.js
@@ -2,7 +2,7 @@
  * Created by hakan on 12/02/2017.
  */
 import React from 'react'
-import {Nav,Navbar,NavItem,MenuItem,NavDropdown,Tooltip,OverlayTrigger} from 'react-bootstrap';
+import {MenuItem, Nav, Navbar, NavDropdown, NavItem, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 class Header extends React.Component {
@@ -29,8 +29,8 @@ class Header extends React.Component {
                     <MenuItem eventKey={1.1} href="https://docs.docker.com/engine/api/" target="_blank">Docker Remote
                         API</MenuItem>
                     <MenuItem divider/>
-                    <MenuItem eventKey={1.2} href="https://docs.docker.com/engine/api/v1.30/"
-                              target="_blank">v1.30</MenuItem>
+                    <MenuItem eventKey={1.2} href="https://docs.docker.com/engine/api/v1.32/"
+                              target="_blank">v1.32</MenuItem>
                     <MenuItem divider/>
                     <MenuItem eventKey={1.3} href="https://docs.docker.com/engine/faq/" target="_blank">FAQ</MenuItem>
                 </NavDropdown>
